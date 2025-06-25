16e6aced1c521477bd46cfc913443baeab937f71
Ariel200609 <arielmontoya200609@gmail.com>
2025-06-24 23:04:13 -0300

    pagina para presentacion

---
+++ js/controller.js
@@ -0,0 +1,38 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const content = document.getElementById("content");
+
+  sections.forEach(section => {
+    const sec = document.createElement("section");
+    sec.classList.add("fade-section");
+
+    const h2 = document.createElement("h2");
+    h2.textContent = section.title;
+
+    const p = document.createElement("p");
+    p.textContent = section.text;
+
+    const img = document.createElement("img");
+    img.src = section.image;
+    img.alt = section.title;
+    img.style.width = "100%";
+    img.style.borderRadius = "12px";
+    img.style.margin = "20px 0";
+
+    sec.appendChild(h2);
+    sec.appendChild(img);
+    sec.appendChild(p);
+
+    content.appendChild(sec);
+  });
+
+  const allSections = document.querySelectorAll("section");
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      }
+    });
+  }, { threshold: 0.1 });
+
+  allSections.forEach(sec => observer.observe(sec));
+});
---
+++ js/model.js
@@ -0,0 +1,22 @@
+const sections = [
+  {
+    title: "The Problem",
+    text: "Today, we all have the same problem. We know that currently it's very difficult to pay attention with the social media. Our phones, tablets, and computers are full of apps, notifications, and websites that are made to get our attention, especially when we are doing something important like studying. ",
+    image: "./assets/problema.jpg" 
+  },
+  {
+    title: "Our Solution",
+    text: "Our solution is Distract-off. It's a smart app with artificial intelligence that helps you stay focused. It blocks the apps and notifications that take your attention away. It also gives you points each time you use the app.",
+    image:"assets/solucion.jpg" 
+  },
+  {
+    title: "Gamification & Rewards",
+    text: "These points can be exchanged for real rewards, like discounts, gift cards, or special offers. Also if you love video-games or competitions you can invite friends (or virtual friends) to use the application and complete daily goals, and get more points to obtain more benefits. ",
+    image: "assets/recompensas.jpg" 
+  },
+  {
+    title: "Final Message",
+    text: "We believe staying focused should not feel like a fight. With Distract-off, your device becomes a tool that helps you, not a problem that stops you. We invite you to use Distract-off free for two weeks. Take back your time. Focus. and get rewarded for it.",
+    image: "assets/ganador.jpg" 
+  }
+];
